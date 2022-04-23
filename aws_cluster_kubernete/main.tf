567be9c46f192ca48ab4fb731a4910354590efd1
Frank Josué Vigil Vega <frankjosue.vigilvega@gmail.com>
2022-04-23 02:25:24 +0200

    creacion de los Deployment con acceso por loadbalance de cenit-configuration

---
+++ aws_cluster_kubernete/main.tf
@@ -0,0 +1,205 @@
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+
+  cluster_name                    = local.cluster_name
+  cluster_version                 = "1.22"
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns = {
+      resolve_conflicts = "OVERWRITE"
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      resolve_conflicts = "OVERWRITE"
+    }
+  }
+
+  cluster_encryption_config = [{
+    provider_key_arn = aws_kms_key.eks.arn
+    resources        = ["secrets"]
+  }]
+
+  vpc_id = module.vpc.vpc_id
+  #subnet_ids = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
+  subnet_ids = module.vpc.private_subnets
+  # Self Managed Node Group(s)
+  self_managed_node_group_defaults = {
+    instance_type                          = "t3.medium"
+    update_launch_template_default_version = true
+    #iam_role_additional_policies           = [aws_iam_role.nodes.arn]
+    iam_role_additional_policies = [
+      "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
+      #"arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+      "arn:aws:iam::aws:policy/AdministratorAccess",
+      #"arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+      #"arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+      "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess",
+      "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+      "arn:aws:iam::aws:policy/IAMFullAccess",
+      #"arn:aws:iam::aws:policy/AutoScalingFullAccess",
+      "arn:aws:iam::aws:policy/AmazonVPCFullAccess",
+      "arn:aws:iam::aws:policy/AmazonEKSServicePolicy",
+      "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+    ]
+  }
+
+  self_managed_node_groups = {
+    one = {
+      name = "cenit-1"
+
+      public_ip    = true
+      max_size     = 5
+      desired_size = 2
+
+      use_mixed_instances_policy = true
+      mixed_instances_policy = {
+        instances_distribution = {
+          on_demand_base_capacity                  = 0
+          on_demand_percentage_above_base_capacity = 10
+          spot_allocation_strategy                 = "capacity-optimized"
+        }
+
+        override = [
+          {
+            instance_type     = "t3.medium"
+            weighted_capacity = "1"
+          }
+        ]
+      }
+
+      pre_bootstrap_user_data = <<-EOT
+      echo "foo"
+      export FOO=bar
+      EOT
+
+      bootstrap_extra_args = "--kubelet-extra-args '--node-labels=node.kubernetes.io/lifecycle=spot'"
+
+      post_bootstrap_user_data = <<-EOT
+      cd /tmp
+      sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+      sudo systemctl enable amazon-ssm-agent
+      sudo systemctl start amazon-ssm-agent
+      EOT
+    }
+  }
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    ami_type               = "AL2_x86_64"
+    disk_size              = 50
+    instance_types         = ["t3.micro", "t3.small", "t3.medium", "t3.large", "t3.xlarge", "t3.2xlarge"]
+    vpc_security_group_ids = [aws_security_group.additional.id]
+  }
+
+  eks_managed_node_groups = {
+    blue = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 1
+
+      instance_types = ["t3.medium"]
+      capacity_type  = "SPOT"
+      labels = {
+        Environment = "test"
+        GithubRepo  = "terraform-aws-eks"
+        GithubOrg   = "terraform-aws-modules"
+      }
+      /*taints = {
+          dedicated = {
+            key    = "dedicated"
+            value  = "gpuGroup"
+            effect = "NO_SCHEDULE"
+          } 
+        }*/
+      tags = {
+        ExtraTag = "example"
+      }
+    }
+    green = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 1
+
+      instance_types = ["t3.medium"]
+      capacity_type  = "SPOT"
+      labels = {
+        Environment = "test"
+        GithubRepo  = "terraform-aws-eks"
+        GithubOrg   = "terraform-aws-modules"
+      }
+      /*taints = {
+         dedicated = {
+          key    = "dedicated"
+          value  = "gpuGroup"
+          effect = "NO_SCHEDULE"
+        } 
+      }*/
+      tags = {
+        ExtraTag = "example"
+      }
+    }
+  }
+
+  # Fargate Profile(s)
+  fargate_profiles = {
+    default = {
+      name = "default"
+      selectors = [
+        {
+          namespace = "kube-system"
+          labels = {
+            k8s-app = "kube-dns"
+          }
+        },
+        {
+          namespace = "default"
+        }
+      ]
+
+      tags = {
+        Owner = "test"
+      }
+
+      timeouts = {
+        create = "20m"
+        delete = "20m"
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+resource "null_resource" "kubeconfig" {
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --region ${var.region} --name ${local.cluster_name}"
+  }
+  depends_on = [
+    module.eks
+  ]
+}
+
+resource "null_resource" "kubernetes_config" {
+
+  provisioner "local-exec" {
+    command = "touch ./../kubeconfig.yaml && cat ~/.kube/config >> ./../kubeconfig.yaml"
+  }
+  depends_on = [
+    null_resource.kubeconfig
+  ]
+}
+resource "null_resource" "delete_kubernetes_config" {
+
+  provisioner "local-exec" {
+    when = "destroy"
+    command = "rm ./../kubeconfig.yaml"
+  }
+  
+  
+}
+ 
---
+++ aws_cluster_kubernete/variables.tf
@@ -0,0 +1,21 @@
+variable "cidr" {
+  description = "Direccion IP privados a utilizar VPC de AWS"
+  default = "10.0.0.0/20"
+}
+
+variable "ssh_pub_path" {
+  description = "Direccion de las llabes ssh publica"
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "cluster_name" {
+  default = "ceint"
+}
+
+variable "region" {
+  default = "eu-central-1"
+}
+
+variable "environment" {
+  default = "testing"
+}
